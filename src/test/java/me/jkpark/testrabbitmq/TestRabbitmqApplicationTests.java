258b1d737d3b5813b69e8642a0f5d6fb52d69e81
Joo-Kwang Park <iam@jkpark.me>
2021-01-10 23:37:27 +0900

    RabbitMQ Basic Setting and Message Listening

---
+++ src/test/java/me/jkpark/testrabbitmq/TestRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jkpark.testrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
